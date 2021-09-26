81fa2fec319199d002d0d277bfdd105f1b91c9b3
Tom Badaczewski <tombadaczewski@gmail.com>
2021-09-26 13:55:22 -0500

    Added player_choice function

--- tictactoe.py
+++ tictactoe.py
@@ -77,11 +77,14 @@ def win_check(board,mark):
     (board[7] == board[5] == board[3] == mark))
 
 
-
+def player_choice(board):
+    position = 0
+    while position is not in [1,2,3,4,5,6,7,8,9] or not space_check(board, position):
+        position = int(input('Choose a position: (1-9) '))
+    return position
 #player1_marker, player2_marker = player_marker_input()
 
 def replay():
     choice = input("Do you want to play again? (Y/N) ")
-    return choice == 'Yes'
+    return choice == 'Y'
 
-replay()
